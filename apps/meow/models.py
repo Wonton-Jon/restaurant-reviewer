e5bb6d9739aca24f6b9dc758bf33fa9e48fbdc1e
Jonathan Aguirre <126916137+jonathanaguirre-ucsc@users.noreply.github.com>
2023-05-24 16:51:18 -0700

    add some restaurants to db if empty

--- apps/meow/models.py
+++ apps/meow/models.py
@@ -18,13 +18,88 @@ def get_username():
 def get_time():
     return datetime.datetime.utcnow()
 
-
 db.define_table(
-    "follow",
-    Field("follower", "reference auth_user"),
-    Field("followed", "reference auth_user"),
+    "restaurant",
+    Field("name"),
+    Field("city"),
+    Field("zipCode", "integer"),
+    Field("rating", default=0.0),
+    Field("number_of_reviews", default=0),
+    Field("cuisine"),
+    Field("is_fastfood", default=False)
 )
 
+db.define_table(
+    'tier_list',
+    Field('user_email', default=get_user_email), #user email that added the restaurant
+    Field('restaurant_id'), #from db.restaurants.id
+    # Field('created_by', 'reference auth_user',
+    #       default=lambda: session.user_id),
+)
+
+if db(db.restaurant).isempty():
+    restaurants = [
+        { 
+            "name": "Hula\'s Island Grill", 
+            "city": "Santa Cruz",
+            "zipCode": 95060,
+            "rating": 4.4,
+            "number_of_reviews": 2096,
+            "cuisine": "Hawaiian",
+            "is_fastfood": False            
+        },
+        { 
+            "name": "Jack\'s Hamburgers", 
+            "city": "Santa Cruz",
+            "zipCode": 95060,
+            "rating": 4.5,
+            "number_of_reviews": 977,
+            "cuisine": "American",
+            "is_fastfood": True            
+        },
+        { 
+            "name": "Taqueria Los Pericos", 
+            "city": "Santa Cruz",
+            "zipCode": 95060,
+            "rating": 4.6,
+            "number_of_reviews": 2096,
+            "cuisine": "Mexican",
+            "is_fastfood": True            
+        },
+        { 
+            "name": "Jack in the Box", 
+            "city": "Santa Cruz",
+            "zipCode": 95060,
+            "rating": 3.6,
+            "number_of_reviews": 1623,
+            "cuisine": "American",
+            "is_fastfood": True            
+        },
+        { 
+            "name": "Ideal Bar & Grill", 
+            "city": "Santa Cruz",
+            "zipCode": 95060,
+            "rating": 4.0,
+            "number_of_reviews": 2317,
+            "cuisine": "American",
+            "is_fastfood": False            
+        }
+    ]
+
+    for restaurant in restaurants:
+        db.restaurant.insert(name=restaurant['name'],
+                             city=restaurant['city'],
+                             zipCode=restaurant['zipCode'],
+                             rating=restaurant['rating'],
+                             number_of_reviews=restaurant['number_of_reviews'],
+                             cuisine=restaurant['cuisine'],
+                             is_fastfood=restaurant['is_fastfood'])
+
+# db.define_table(
+#     "follow",
+#     Field("follower", "reference auth_user"),
+#     Field("followed", "reference auth_user"),
+# )
 db.commit()
 
 def add_users_for_testing(num_users):
