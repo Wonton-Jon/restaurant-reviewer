2547a7b5643e01465b5fd172d9f676395c7dd9e0
Jonathan Aguirre <60633893+Wonton-Jon@users.noreply.github.com>
2023-06-01 13:46:42 -0700

    remove unused code

--- project/apps/restaurant_reviewer/controllers.py
+++ project/apps/restaurant_reviewer/controllers.py
@@ -60,30 +60,8 @@ def index():
         filter_restaurants_url = URL('filter_restaurants', signer=url_signer),
         get_restaurants_url = URL('get_restaurants', signer=url_signer),
         # COMPLETE: return here any signed URLs you need.
-        get_users_url = URL('get_users', signer=url_signer),
         follow_url=URL('set_follow', signer=url_signer),
     )
-
-@action("get_users")
-@action.uses(db, auth.user)
-def get_users():
-    #Get the list of ids of users followed
-    users_followed = db(db.follow.follower == auth.current_user.get("id")).select(db.follow.followed).as_list()
-    ids_followed = []
-    for user in users_followed:
-        ids_followed.append(user["followed"])
-
-    #Create a dictionary with format (k,v) => (username : Boolean) 
-    #True if followed false otherwise
-    following = db(db.auth_user.id.belongs(ids_followed)).select(db.auth_user.username).as_list()
-    for user in following:
-        user['isFollowing'] = True
-    
-    #Get list of unfollowed users
-    not_following = db(~db.auth_user.id.belongs(ids_followed)).select(db.auth_user.username).as_list()
-    for user in not_following:
-        user['isFollowing'] = False
-    return dict(followed=following, unfollowed=not_following)
 
 
 @action("set_follow", method="POST")
@@ -125,7 +103,13 @@ def filter_users():
 def get_restaurants():
     #Get the list of ids of restaurants
     restaurants = db(db.restaurant).select(orderby=~db.restaurant.rating).as_list()
+
+    #Get the restaurants that the user is following
+
+
     return dict(restaurants=restaurants)
+
+
 
 @action("filter_restaurants", method="GET")
 @action.uses(db)
